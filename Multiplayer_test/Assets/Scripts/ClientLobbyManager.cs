ace9d957c17950690342b915d8a98ec690309afe
Nick <nickmbaker1@gmail.com>
2020-06-16 14:09:31 -0400

    Implemented a simple player cursor for the Lobby UI.

---
+++ Multiplayer_test/Assets/Scripts/ClientLobbyManager.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Mirror;
+public class ClientLobbyManager : NetworkBehaviour
+{
+    private Canvas LobbyUICanvas;
+    public GameObject PlayerLobbyCursorPrefab;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        LobbyUICanvas = GameObject.FindObjectOfType<Canvas>();
+        ClientScene.RegisterPrefab(PlayerLobbyCursorPrefab);
+    }
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            GameObject spawnedCursor = Instantiate(PlayerLobbyCursorPrefab, LobbyUICanvas.transform);
+            NetworkServer.Spawn(spawnedCursor);
+        }
+    }
+}
---
+++ Multiplayer_test/Assets/Scripts/PlayerLobbyCursor.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Mirror;
+public class PlayerLobbyCursor : NetworkBehaviour
+{
+    private Color32[] cursorColors = {new Color32(255, 40, 40, 255), new Color32(17, 61, 255, 255), new Color32(255, 246, 9, 255), new Color32(0, 188, 37, 255),
+    new Color32(252, 156, 2, 255), new Color32(135, 19, 193, 255), new Color32(255, 0, 238, 255), new Color32(2, 214, 221, 255), new Color32(40,40,40, 255)};
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        transform.SetParent(GameObject.FindObjectOfType<Canvas>().transform);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (!isLocalPlayer)
+            return;
+
+        if (Input.GetKey(KeyCode.W))
+        {
+            transform.position = new Vector3(transform.position.x, transform.position.y + 5, 0);
+        }
+        if (Input.GetKey(KeyCode.S))
+        {
+            transform.position = new Vector3(transform.position.x, transform.position.y - 5, 0);
+        }
+        if (Input.GetKey(KeyCode.A))
+        {
+            transform.position = new Vector3(transform.position.x - 5, transform.position.y, 0);
+        }
+        if (Input.GetKey(KeyCode.D))
+        {
+            transform.position = new Vector3(transform.position.x + 5, transform.position.y, 0);
+        }
+    }
+}
