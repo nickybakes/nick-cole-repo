4bb20e2305cb7866e3d6bd94bb28df8b75db8b65
Nick <nickmbaker1@gmail.com>
2020-07-24 17:16:28 -0400

    More work on movement, trying to get slopes working.

--- Multiplayer_test/Assets/Scripts/NeonHeightsMovement.cs
+++ Multiplayer_test/Assets/Scripts/NeonHeightsMovement.cs
@@ -7,25 +7,65 @@ public class NeonHeightsMovement : MonoBehaviour
 {
     BoxCollider2D thisCollider;
     Rigidbody2D rigidBody;
+    SpriteRenderer spriteRenderer;
+
+    private Vector2 inputVector;
+    private Vector2 contactNormal;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        this.rigidBody = GetComponent<Rigidbody2D>();
+        this.thisCollider = GetComponent<BoxCollider2D>();
+        this.spriteRenderer = GetComponent<SpriteRenderer>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        Camera.main.transform.position = new Vector3(transform.position.x, transform.position.y, Camera.main.transform.position.z);
+        if (inputVector.x > 0)
+        {
+            spriteRenderer.flipX = false;
+            print(contactNormal.normalized);
+            //SetVelocity(5*contactNormal.normalized.y, 5*contactNormal.normalized.x);
+            SetVelocity(5, rigidBody.velocity.y);
+        }
+        else if (inputVector.x < 0)
+        {
+            spriteRenderer.flipX = true;
+            print(contactNormal.normalized);
+            //SetVelocity(-5 * contactNormal.normalized.y, -5 * contactNormal.normalized.x);
+            SetVelocity(-5, rigidBody.velocity.y);
+        }
+
+    }
+
+    void SetVelocity(float x, float y)
+    {
+        rigidBody.velocity = new Vector2(x, y);
+    }
+
+    public void OnMove(InputValue value)
+    {
+
+        if (!Application.isFocused)
+            return;
+
+        inputVector = value.Get<Vector2>();
+    }
+
+    public void OnSelect()
+    {
+        SetVelocity(rigidBody.velocity.x, 10);
     }
 
     void OnCollisionStay2D(Collision2D collision)
     {
         foreach (ContactPoint2D contact in collision.contacts)
         {
-            print(contact.collider.name + " hit " + contact.otherCollider.name);
             // Visualize the contact point
+            contactNormal = contact.normal;
             Debug.DrawRay(contact.point, contact.normal, Color.white);
         }
     }
