baee1bb4d1678fb81d83770363037fc63e957f7b
Nick <nickmbaker1@gmail.com>
2020-07-24 14:14:31 -0400

    Started working on movement stuff

---
+++ Multiplayer_test/Assets/Scripts/NeonHeightsMovement.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class NeonHeightsMovement : MonoBehaviour
+{
+    BoxCollider2D thisCollider;
+    Rigidbody2D rigidBody;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void OnCollisionStay2D(Collision2D collision)
+    {
+        foreach (ContactPoint2D contact in collision.contacts)
+        {
+            print(contact.collider.name + " hit " + contact.otherCollider.name);
+            // Visualize the contact point
+            Debug.DrawRay(contact.point, contact.normal, Color.white);
+        }
+    }
+}
