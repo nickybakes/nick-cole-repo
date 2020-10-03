4706eeb2cfc35b7b25d015065a7fdccd8658a6a1
Nick <nickmbaker1@gmail.com>
2020-10-02 21:00:55 -0400

    made a quad renderer

---
+++ TextureWarp/TextureWarp/Game1.cs
@@ -0,0 +1,179 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+
+namespace TextureWarp
+{
+    /// <summary>
+    /// This is the main type for your game.
+    /// </summary>
+    public class Game1 : Game
+    {
+        public static string debug = "";
+
+        GraphicsDeviceManager graphics;
+        SpriteBatch spriteBatch;
+
+        private int nudgeAmount = 60;
+
+        private Texture2D whiteSquare;
+
+        private Texture2D vertHandleTexture;
+
+        private Texture2D edgeTexture;
+
+        private ShapeDrawer drawer;
+        private Quad quad;
+
+        private KeyboardState kbState;
+        private KeyboardState previouskbState;
+        private SpriteFont arial18;
+
+        private Texture2D checkerTexture;
+
+        private RenderTarget2D finalRenderTarget;
+
+        public Game1()
+        {
+            graphics = new GraphicsDeviceManager(this);
+            Content.RootDirectory = "Content";
+        }
+
+        /// <summary>
+        /// Allows the game to perform any initialization it needs to before starting to run.
+        /// This is where it can query for any required services and load any non-graphic
+        /// related content.  Calling base.Initialize will enumerate through any components
+        /// and initialize them as well.
+        /// </summary>
+        protected override void Initialize()
+        {
+            // TODO: Add your initialization logic here
+            this.IsMouseVisible = true;
+
+            //setting window properties. it can be resized, but at first we want it at a predetermined size
+            Window.AllowUserResizing = true;
+            graphics.PreferredBackBufferHeight = 1080;
+            graphics.PreferredBackBufferWidth = 1920;
+            System.Windows.Forms.Form form = (System.Windows.Forms.Form)System.Windows.Forms.Form.FromHandle(Window.Handle);
+            form.WindowState = System.Windows.Forms.FormWindowState.Maximized;
+
+            finalRenderTarget = new RenderTarget2D(GraphicsDevice, 1920, 1080);
+            vertHandleTexture = CreateTexture(GraphicsDevice, 32, 32, Color.White);
+            edgeTexture = CreateTexture(GraphicsDevice, 8, 8, Color.Yellow);
+            whiteSquare = CreateTexture(GraphicsDevice, 4, 4, Color.White);
+
+            base.Initialize();
+        }
+
+        /// <summary>
+        /// LoadContent will be called once per game and is the place to load
+        /// all of your content.
+        /// </summary>
+        protected override void LoadContent()
+        {
+            // Create a new SpriteBatch, which can be used to draw textures.
+            spriteBatch = new SpriteBatch(GraphicsDevice);
+
+            arial18 = Content.Load<SpriteFont>("Arial18");
+            checkerTexture = Content.Load<Texture2D>("checker");
+
+            quad = new Quad(new Vector2(200, 30), new Vector2(1200, 70), new Vector2(250, 800), new Vector2(1200, 600), checkerTexture);
+            //quad = new Quad(new Vector2(0, 0), new Vector2(1920, 0), new Vector2(0, 1080), new Vector2(1920, 1080), checkerTexture);
+            // TODO: use this.Content to load your game content here
+        }
+
+        /// <summary>
+        /// UnloadContent will be called once per game and is the place to unload
+        /// game-specific content.
+        /// </summary>
+        protected override void UnloadContent()
+        {
+            // TODO: Unload any non ContentManager content here
+        }
+
+        /// <summary>
+        /// Allows the game to run logic such as updating the world,
+        /// checking for collisions, gathering input, and playing audio.
+        /// </summary>
+        /// <param name="gameTime">Provides a snapshot of timing values.</param>
+        protected override void Update(GameTime gameTime)
+        {
+            if (GamePad.GetState(PlayerIndex.One).Buttons.Back == ButtonState.Pressed || Keyboard.GetState().IsKeyDown(Keys.Escape))
+                Exit();
+
+            previouskbState = kbState;
+            kbState = Keyboard.GetState();
+
+            if (kbState.IsKeyDown(Keys.W) && previouskbState.IsKeyUp(Keys.W))
+                quad.verts[0].Y -= nudgeAmount;
+            if (kbState.IsKeyDown(Keys.S) && previouskbState.IsKeyUp(Keys.S))
+                quad.verts[0].Y += nudgeAmount;
+            if (kbState.IsKeyDown(Keys.A) && previouskbState.IsKeyUp(Keys.A))
+                quad.verts[0].X -= nudgeAmount;
+            if (kbState.IsKeyDown(Keys.D) && previouskbState.IsKeyUp(Keys.D))
+                quad.verts[0].X += nudgeAmount;
+
+
+            base.Update(gameTime);
+        }
+
+        /// <summary>
+        /// This is called when the game should draw itself.
+        /// </summary>
+        /// <param name="gameTime">Provides a snapshot of timing values.</param>
+        protected override void Draw(GameTime gameTime)
+        {
+
+            GraphicsDevice.SetRenderTarget(finalRenderTarget);
+
+            GraphicsDevice.Clear(Color.CornflowerBlue);
+
+            spriteBatch.Begin();
+
+            //quad.DrawVerts(spriteBatch, vertHandleTexture);
+
+            //quad.DrawEdges(spriteBatch, edgeTexture);
+
+            quad.DrawQuad(spriteBatch, whiteSquare);
+
+            spriteBatch.End();
+
+            GraphicsDevice.SetRenderTarget(null);
+
+            spriteBatch.Begin();
+
+            spriteBatch.Draw(finalRenderTarget, new Rectangle(0, 0, GraphicsDevice.Viewport.Width, GraphicsDevice.Viewport.Height), Color.White);
+
+            spriteBatch.DrawString(arial18, debug, Vector2.Zero, Color.White);
+            spriteBatch.End();
+
+            // TODO: Add your drawing code here
+
+            base.Draw(gameTime);
+        }
+
+        public static Texture2D CreateTexture(GraphicsDevice device, int width, int height, Color color)
+        {
+            //initialize a texture
+            Texture2D texture = new Texture2D(device, width, height);
+
+            //the array holds the color for each pixel in the texture
+            Color[] data = new Color[width * height];
+            for (int pixel = 0; pixel < data.Length; pixel++)
+            {
+                //the function applies the color according to the specified pixel
+                data[pixel] = color;
+            }
+
+            //set the color
+            texture.SetData(data);
+
+            return texture;
+        }
+    }
+}
---
+++ TextureWarp/TextureWarp/Quad.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+
+
+namespace TextureWarp
+{
+    class Quad
+    {
+        public Vector2[] verts;
+        public Texture2D quadTexture;
+        public Color[,] textureData;
+
+        public Quad(Vector2 topLeft, Vector2 topRight, Vector2 bottomLeft, Vector2 bottomRight, Texture2D quadTexture)
+        {
+            verts = new[] { topLeft, topRight, bottomLeft, bottomRight };
+            this.quadTexture = quadTexture;
+            Color[] textureDataRaw = new Color[this.quadTexture.Width * this.quadTexture.Height];
+            quadTexture.GetData<Color>(textureDataRaw);
+
+            textureData = new Color[quadTexture.Width, quadTexture.Height];
+            for (int y = 0; y < quadTexture.Height; y++)
+            {
+                for (int x = 0; x < quadTexture.Width; x++)
+                {
+                    // Assumes row major ordering of the array.
+                    textureData[x, y] = textureDataRaw[y * quadTexture.Height + x];
+                }
+            }
+        }
+
+        public void DrawVerts(SpriteBatch spriteBatch, Texture2D texture)
+        {
+            for(int i = 0; i < verts.Length; i++)
+            {
+                spriteBatch.Draw(texture, new Vector2(verts[i].X - texture.Width/2, verts[i].Y - texture.Height/2), Color.White);
+            }
+        }
+
+        public void DrawEdges(SpriteBatch spriteBatch, Texture2D texture)
+        {
+            DrawLine(spriteBatch, verts[0], verts[1], texture);
+            DrawLine(spriteBatch, verts[0], verts[2], texture);
+            DrawLine(spriteBatch, verts[1], verts[3], texture);
+            DrawLine(spriteBatch, verts[2], verts[3], texture);
+        }
+
+        public void DrawQuad(SpriteBatch spriteBatch, Texture2D square)
+        {
+            //float i1 = 0;
+            //float j1 = 0;
+            //int slopeX1 = (int) InterpSlope(Slope(verts[0], verts[1]), Slope(verts[2], verts[3]), i1);
+            //int slopeY1 = (int) InterpSlope(Slope(verts[0], verts[2]), Slope(verts[1], verts[3]), j1);
+            //Game1.debug = ((Slope(verts[0], verts[1]))).ToString() + ", " + (slopeY1).ToString();
+            for (float i = 0; i <= 1; i += .002f)
+            {
+                Vector2 leftLine = verts[2] - verts[0];
+                Vector2 rightLine = verts[3] - verts[1];
+                Vector2 leftPoint = leftLine * i + verts[0];
+                Vector2 rightPoint = rightLine * i + verts[1];
+                Vector2 finalLine = rightPoint - leftPoint;
+                for (float j = 0; j <= 1; j += .001f)
+                {
+                    spriteBatch.Draw(square, new Vector2((int)(finalLine.X * j + leftPoint.X),
+                        (int)(finalLine.Y * j + leftPoint.Y)), textureData[(int)(j*quadTexture.Width), (int)(i * quadTexture.Height)]);
+                }
+            }
+        }
+
+        private float InterpSlope(float slope1, float slope2, float interp)
+        {
+            return (slope1 * interp) + (slope2 * (1 - interp));
+        }
+
+        private float Slope(Vector2 start, Vector2 end)
+        {
+            return (end.Y - start.Y)/(end.X - start.X);
+        }
+
+        private void DrawLine(SpriteBatch spriteBatch, Vector2 start, Vector2 end, Texture2D texture)
+        {
+            Vector2 lineToStart = end - start;
+
+            for (float i = 0; i < 1; i += .005f)
+            {
+                //increment throug the line and draw small sprites to generate a visual line
+                spriteBatch.Draw(texture, new Vector2((int)(lineToStart.X * i + start.X),
+                    (int)(lineToStart.Y * i + start.Y)), Color.White);
+            }
+        }
+
+    }
+}
